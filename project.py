30ec4f93f6398304201e8ccae1c7c85ab47d005b
margarita2104 <margarita.p.ferreira@gmail.com>
2024-11-25 17:31:28 +0100

    fixed conflicts, merging user interface into main

--- project.py
+++ project.py
@@ -1,7 +1,7 @@
 import requests  # Import requests library
 from pprint import pprint  # Import pretty print
 
-meal_db_api_base_url = 'https://www.themealdb.com/api/json/v1/1/'  # Store themealdb base url in new variable
+meal_db_api_base_url = 'https://www.themealdb.com/api/json/v1/1/'  # Store themealdb base URL in a new variable
 
 user_name = input('Hi! Welcome to the Meal Planner. Please, tell us your name :) ')
 print('Nice to meet you, {}! '.format(user_name))
@@ -21,20 +21,45 @@ def search_meal_by_name():
         print(meal['strInstructions'])
 
 
-def search_ingredient():  # Create a function that makes a request to the Themealdb API with the required ingredient as part of the search query
+def search_ingredient():
     ingredient_name = input("What is the ingredient you would like to search for? ").strip()
-    ingredient_filter_path = 'filter.php?i='  # Store the ingredient filter path in new variable
-    search_url = f"{meal_db_api_base_url}{ingredient_filter_path}{ingredient_name}"  # Store the search url in new variable, including base + filter + the ingredient the user selected. Used the strip method on the ingredient_name variable in order to remove leading and trailing white spaces
-    response = requests.get(
-        search_url)  # Use the requests library to ask for this information using the search_url variable
-    data = response.json()  # Turn into json object that we can work with and store in new variable called data
-    # print(data['meals'])  #Print out the response in order to see the list of returned recipes and their properties
+    ingredient_filter_path = 'filter.php?i='
+    search_url = f"{meal_db_api_base_url}{ingredient_filter_path}{ingredient_name}"
+    response = requests.get(search_url)
+    data = response.json()
+
     if data['meals'] is None:
         print(f"No meals found with ingredient '{ingredient_name}'. Please try another ingredient.")
     else:
-        for meal in data['meals']:
+        meals = data['meals']
+        recipe = []
+        for meal in meals:
             pprint(meal['strMeal'])
             print(f"Meal ID: {meal['idMeal']}")
+            recipe.append(meal['strMeal'])
+            idmeal = meal['idMeal']
+            mealdetail = get_meal_details(idmeal)
+            instruction = mealdetail.get('strInstructions', 'No instructions available.')
+            print("\nInstructions:")
+            print(instruction)
+            recipe.append(instruction)
+
+        download_choice = input('Would you like to save the results into a txt file? (y/n): ')
+        if download_choice.strip().lower() == 'y':
+            with open('themealdb.txt', 'w+', encoding='utf-8') as text_file:
+                for line in recipe:
+                    text_file.write(f'{line}\n')
+            print("Results saved to 'themealdb.txt'.")
+        else:
+            print('You chose not to download and save the recipes.')
+
+
+def get_meal_details(meal_id):
+    meal_lookup_path = f"lookup.php?i={meal_id}"
+    search_url = f"{meal_db_api_base_url}{meal_lookup_path}"
+    response = requests.get(search_url)
+    data = response.json()
+    return data['meals'][0] if data['meals'] else {}
 
 
 def random_meal():
@@ -70,30 +95,3 @@ def menu():
 
 
 menu()
-
-#
-# def search_meal():  # Create a function that makes a request to the Themealdb API with the a specific meal id to get the full details of a meal
-#     meal_lookup_path = 'lookup.php?i='  # Store the ingredient lookup path in new variable
-#     search_url = '{}{}{}'.format(meal_db_api_base_url, meal_lookup_path,
-#                                  idmeal)  # Store the search url in new variable, including base + filter + the meal id pertaining to the ingredient the user selected.
-#     response = requests.get(
-#         search_url)  # Use the requests library to ask for this information using the search_url variable
-#     data = response.json()  # Turn into json object that we can work with and store in new variable called data
-#     # print(data['meals'])  #Print out the response in order to see the list of returned recipes and their properties
-#     return data['meals']  # Return the response
-#
-#
-# meals = search_ingredient()  # Store the search result in a new variable called meals
-# if meals is None:  # Check if we received any data at all. if not, then
-#     print('No meals found with ingredient {}. Please choose another one.'.format(
-#         ingredient_name))  # Print out a message to the user and ask to pick another ingredient
-# else:
-#     for meal in meals:  # If the ingredient is found, then
-#         pprint(meal['strMeal'])  # Print out the name of the meal
-#         # pprint(meal['idMeal'])  #Print out the id of the meal
-#         idmeal = meal['idMeal']  # Create a new variable called idmeal to get the meal  id
-#         mealdetail = search_meal()[
-#             0]  # Get the full meal details from the meal id for the first item and store in new variable called mealdetail
-#         # pprint(mealdetail) #Print out the full meal details for each meal containing the ingredient selected by user
-#
-#         print(mealdetail['strInstructions'])  # Print the instructions
